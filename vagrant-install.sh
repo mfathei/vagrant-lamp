1796cac536a87ec173faa0225f871f687e072859
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-11-17 15:47:42 +0200

    generate ssh key and install php-xml extension

--- vagrant-install.sh
+++ vagrant-install.sh
@@ -6,6 +6,13 @@ apt-get upgrade
 # Basic Linux Stuff
 apt-get install -y git
 
+# SSH key
+ssh-keygen -t rsa -b 4096 -C "oracle.dev10g@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+git config --global user.name "Mohamed Mahdy"
+git config --global user.email "oracle.dev10g@gmail.com"
+
 # Apache
 apt-get install -y apache2
 
@@ -30,12 +37,12 @@ apt-get install -y php7.2-common
 apt-get install -y php7.2-mbstring
 apt-get install -y php7.2-mcrypt
 apt-get install -y php7.2-zip
+apt-get install -y php-xml
 
 # Composer
 cd ~
-apt-get install -y curl php-cli unzip
+apt-get install -y curl unzip
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-
 
 # Set MySQL Pass
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
