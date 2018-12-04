62f312604df48cfefddff3024a5527c744b0b610
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-12-04 19:42:38 +0200

    enablw ssh in centos 7

--- boxes/centos/centos-install.sh
+++ boxes/centos/centos-install.sh
@@ -4,6 +4,10 @@ yum update -y
 # disable selinux
 sudo setenforce 0
 
+# SSH
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo systemctl restart sshd;
+
 # nano 
 sudo yum install -y nano
 
